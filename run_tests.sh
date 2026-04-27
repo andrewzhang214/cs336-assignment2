31432bf613c1787547a151375256f98f33c9b56f
Andrew Zhang <andrewzhang214@gmail.com>
2026-04-27 09:32:00 +0000

    initialization script added

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+git config --global user.name "Andrew Zhang"
+git config --global user.email "andrewzhang214@gmail.com"
+
+mkdir -p /root/.ssh
+cp -r /workspace/.ssh/ /root/.ssh/
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/id_ed25519
+
 clear
 clear
 
