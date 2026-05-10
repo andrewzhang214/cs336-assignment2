64918d16cff16201a3e18c07438818e27a2b3862
Andrew Zhang <andrewzhang214@gmail.com>
2026-05-10 18:47:52 +0800

    setup

--- startup.sh
+++ startup.sh
@@ -19,7 +19,6 @@ uv sync
 # Git configurations
 git config --global user.name "Andrew Zhang"
 git config --global user.email "andrewzhang214@gmail.com"
-git pull
 
 
 # Github credentials
@@ -27,5 +26,8 @@ mkdir -p /root/.ssh
 cp -r /workspace/.ssh/ /root/
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/id_ed25519
+
+git pull
+
 
 source /root/.bashrc
