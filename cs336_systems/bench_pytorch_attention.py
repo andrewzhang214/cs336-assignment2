625324aee0381eaaf8f3b88991ab8b54cb3f2e46
Andrew Zhang <andrewzhang214@gmail.com>
2026-05-14 17:40:13 +0800

    attention benchmarking

---
+++ cs336_systems/bench_pytorch_attention.py
@@ -0,0 +1,224 @@
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+import numpy as np
+import torch
+
+
+
+from cs336_basics.model import scaled_dot_product_attention
+from cs336_systems.utils import AttentionBenchmarkReporter, AttentionRow
+
+
+def emit_row(args, reporter, f_avg_time, f_std_time, b_avg_time, b_std_time, mem_before_bwd_mb, status, f=".3f"):
+    row = AttentionRow(
+        batch_size=args.batch_size,
+        d_model=args.d_model,
+        seq_length=args.seq_length,
+        f_avg_ms=format(f_avg_time, f),
+        f_std_ms=format(f_std_time, f),
+        b_avg_ms=format(b_avg_time, f),
+        b_std_ms=format(b_std_time, f),
+        mem_before_bwd_mb=format(mem_before_bwd_mb, f),
+        status=status
+    )
+    reporter.append(row)
+
+
+def time_forward(
+        q: torch.Tensor, 
+        k: torch.Tensor,
+        v: torch.Tensor,
+        mask: torch.Tensor
+    ) -> float:
+
+    
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    # Run attn
+    torch.cuda.synchronize()
+    start.record()
+    _ = scaled_dot_product_attention(q, k, v, mask=mask)
+    end.record()
+    torch.cuda.synchronize()
+
+    return start.elapsed_time(end)
+
+
+
+def time_backward(
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        mask: torch.Tensor
+    ) -> float:
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    # Run forward
+    out = scaled_dot_product_attention(q, k, v, mask=mask)
+    loss = out.sum()
+
+    # Begin recording
+    torch.cuda.synchronize()
+    start.record()
+    loss.backward()
+    end.record()
+    torch.cuda.synchronize()
+
+    return start.elapsed_time(end)
+
+
+def run_benchmark_split(args, dtype: torch.dtype):
+
+    # Returns list of times
+        
+    device = torch.device("cuda")
+    q = k = v = mask = None
+    
+    try:
+        # Initialize K,Q,V of size: [batch, seq, d_model]
+        q = torch.randn(args.batch, args.seq_length, args.d_model, device=device, dtype=dtype, requires_grad=True)
+        k = torch.randn(args.batch, args.seq_length, args.d_model, device=device, dtype=dtype, requires_grad=True)
+        v = torch.randn(args.batch, args.seq_length, args.d_model, device=device, dtype=dtype, requires_grad=True)
+        mask = torch.tril(torch.ones((args.seq_length, args.seq_length), device=device, dtype=torch.bool))
+
+        # Run warmup
+        for _ in range(args.num_warmup_steps):
+            # Run forward
+            time_forward(q, k, v, mask)
+
+            # Run backward
+            time_backward(q, k, v, mask)
+
+            q.grad = None
+            k.grad = None
+            v.grad = None
+
+        # Run measure
+        f_times, b_times = [], []
+        for _ in range(args.num_measure_steps):
+            # Run forward
+            f_times.append(time_forward(q, k, v, mask))
+
+
+        # Collect memory
+        mem_before_bwd_mb = torch.cuda.memory_allocated() / (1024 ** 2)
+
+        for _ in range(args.num_measure_steps):
+            # Run backward
+            b_times.append(time_backward(q, k, v, mask))
+
+            q.grad = None
+            k.grad = None
+            v.grad = None
+        
+        return f_times, b_times, mem_before_bwd_mb
+    
+    except torch.OutOfMemoryError:
+        del q, k, v, mask
+        torch.cuda.empty_cache()
+        raise
+
+
+def run_one_setting(args, dtype: torch.dtype, reporter):
+
+    # Collect time and memory statistics and record
+
+    try:
+        local_vars = {}
+        # Run measurements and collect times
+        f_times, b_times, mem_before_bwd_mb = run_benchmark_split(args, dtype)
+
+        
+
+        # Perform statistical analysis
+        f_times = np.array(f_times)
+        b_times = np.array(b_times)
+        f_avg, f_std = np.mean(f_times), np.std(f_times)
+        b_avg, b_std = np.mean(b_times), np.std(b_times)
+
+        # Log the benchmark results with reporter
+        emit_row(args, reporter, f_avg, f_std, b_avg, b_std, mem_before_bwd_mb, "ok")
+
+    except torch.OutOfMemoryError:
+        print(f"OOM Error for model size: {args.model_size}")
+        emit_row(args, reporter, np.nan, np.nan, np.nan, np.nan, np.nan, "oom")
+
+
+def run_sweep(args, dtype: torch.dtype, reporter: AttentionBenchmarkReporter):
+    for d in [int(a) for a in args.sweep_d_model.split(',')]:
+        for s in [int(a) for a in args.sweep_seq_length.split(',')]:
+            torch.cuda.empty_cache()
+            
+            # Update args
+            args.d_model = d
+            args.seq_length = s
+
+            # Run one setting 
+            run_one_setting(args, dtype, reporter)
+            print(f">>> Finish run for model with size {d} and sequence length {s}")
+    
+    # Write to md
+    reporter.render_markdown()
+
+
+def main():
+    parser = ArgumentParser(
+        prog="Pytorch Attention Benchmarker",
+        description="Benchmarks basic pytorch attention implementation"
+    )
+
+    # K,Q,V dimensions
+    parser.add_argument("--batch", type=int, default=8)
+    parser.add_argument("--d-model", type=int, default=32)
+    parser.add_argument("--seq-length", type=int, default=128)
+
+
+    # Data parameters
+    parser.add_argument('--seed', type=int, default=42) # For producing random K,Q,V
+    parser.add_argument("dtype", type=str, default="float32")
+
+    # Measure parameters
+    parser.add_argument('--num-warmup-steps', type=int, default=5)
+    parser.add_argument('--num-measure-steps', type=int, default=100)
+    parser.add_argument('--mode', choices=["inference", "train"], default='train')
+
+    # Sweeper
+    parser.add_argument('--sweep', action="store_true")
+    parser.add_argument('--sweep-d-model', type=str, default="16,32,64,128")
+    parser.add_argument('--sweep-seq-length', type=str, default="256,1024,4096")
+
+    # Reporter
+    parser.add_argument('--out-dir', type=str, default="runs/pytorch_attention")
+
+
+    if not torch.cuda.is_available():
+        raise RuntimeError("CUDA is required for this benchmark.")
+    
+
+    args = parser.parse_args()
+
+
+    torch.manual_seed(args.seed)
+    dtype_map = {"float32": torch.float32, "float16": torch.float16, "bfloat16": torch.bfloat16}
+    dtype = dtype_map[args.dtype]
+
+    # Reporter info
+    out_path = Path(args.out_dir)
+    reporter = AttentionBenchmarkReporter(
+        jsonl_path = out_path / "metrics.jsonl",
+        md_path= out_path / "metrics.md"
+    )
+
+    if args.sweep:
+        run_sweep(args, dtype, reporter)
+
+
+
+
+if __name__ == "__main__":
+    main()
--- cs336_systems/utils.py
+++ cs336_systems/utils.py
@@ -24,8 +24,6 @@ class BenchmarkRow():
 
 
 
-
-
 class BenchmarkReporter():
 
     def __init__(
@@ -57,3 +55,52 @@ class BenchmarkReporter():
         with self.md_path.open('w') as f:
             f.write(df.to_markdown(index=False))
 
+
+
+@dataclass
+class AttentionRow():
+    batch_size: int
+    d_model: int
+    seq_length: int
+    
+    f_avg_ms: str
+    f_std_ms: str
+    b_avg_ms: str
+    b_std_ms: str
+
+    mem_before_bwd_mb: str
+    status: str # "ok" / "oom" / "error:<Type>"
+
+
+
+
+class AttentionBenchmarkReporter():
+
+    def __init__(
+            self,
+            jsonl_path: str | Path,
+            md_path: str | Path
+        ):
+        self.jsonl_path = Path(jsonl_path)
+        self.md_path = Path(md_path)
+
+        # Ensure directories exist
+        self.jsonl_path.parent.mkdir(parents=True, exist_ok=True)
+        self.md_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    def append(self, row: AttentionRow):
+
+        # Append to jsonl
+        with self.jsonl_path.open("a", encoding="utf-8") as f:
+            f.write(json.dumps(asdict(row), ensure_ascii=False) + "\n")
+
+
+
+    def render_markdown(self):
+        # Write Markdown
+
+        # Refresh markdown file
+        # Load JSONL
+        df = pd.read_json(self.jsonl_path, lines=True)
+        with self.md_path.open('w') as f:
+            f.write(df.to_markdown(index=False))
