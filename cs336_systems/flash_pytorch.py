95a496c86491396b232d581809db89fe6bd954fb
Andrew Zhang <andrewzhang214@gmail.com>
2026-05-21 11:17:11 +0800

    flash pytorch

--- cs336_systems/flash_pytorch.py
+++ cs336_systems/flash_pytorch.py
@@ -1,9 +1,8 @@
 
 import math
 
-from einops import einsum, reduce
+from einops import rearrange, einsum, reduce
 import torch
-
 
 
 
@@ -25,12 +24,12 @@ class FlashAttention2Pytorch(torch.autograd.Function):
         scale = math.sqrt(d)
 
         # Initialize output O (batch, seq_len, d)
-        output = torch.empty(batch, seq_len, d, device=device, dtype=torch.float32)
-        logsumexp = torch.empty(batch, seq_len, device=device, dtype=torch.float32)
+        o = torch.empty(batch, seq_len, d, device=device, dtype=torch.float32)
+        L = torch.empty(batch, seq_len, device=device, dtype=torch.float32)
 
-        for i in range(1, Tq+1): # Iterates over sets of query vectors
+        for i in range(Tq): # Iterates over sets of query vectors
             # Load Qi from VRAM (batch, Bq, d)
-            q_tile = q[:, Bq, :]
+            q_tile = q[:, i*Bq:(i+1)*Bq, :]
 
             # Initialize Oi_0 (batch, Bq, d) 
             o_tile = torch.zeros(batch, Bq, d, device=device, dtype=torch.float32)
@@ -39,9 +38,9 @@ class FlashAttention2Pytorch(torch.autograd.Function):
             l = torch.zeros(batch, Bq, device=device, dtype=torch.float32)
 
             # Initialize mi (batch, Bq)  Running max per row (Changes as we go across seq)
-            m = torch.zeros(batch, Bq, device=device, dtype=torch.float32)
+            m = torch.full((batch, Bq), -float("inf"), device=device, dtype=torch.float32)
 
-            for j in range(1, Tk+1): # Iterates over sets of key and value vectors and now iteratively craetes O horizontally across the seq
+            for j in range(Tk): # Iterates over sets of key and value vectors and now iteratively creates O horizontally across the seq
                 # Load Kj and Vj (batch, Bk, d)
                 k_tile = k[:, j*Bk:(j+1)*Bk, :]
                 v_tile = v[:, j*Bk:(j+1)*Bk, :]
@@ -51,15 +50,19 @@ class FlashAttention2Pytorch(torch.autograd.Function):
                 s = einsum(q_tile, k_tile, "... Bq d, ... Bk d -> ... Bq Bk") / scale
 
 
-                # Compute m: Running max per row (batch, Bq)
+                # Compute m: Running max per row (batch, Bq, 1)
                 s_row_max = reduce(s, "... Bq Bk -> ... Bq", "max")
-                m_prev = m
-                m = torch.maximum(s_row_max, m) 
+                m_prev = m.clone()
+                m = rearrange(torch.maximum(s_row_max, m), "... Bq -> ... Bq 1")
+                m_prev = rearrange(m_prev, "... Bq -> ... Bq 1")
 
                 # Compute partial P (batch, Bq, Bk)
                 # This is NOT EXACT since the running max is changing (also it's not normalized yet)
-                p_partial = torch.exp(s - m) # Verify if it broadcasts, otherwise need to expand m to (... Bq 1)
 
+                p_partial = torch.exp(s - m)
+
+                m_prev = rearrange(m_prev, "... Bq 1 -> ... Bq")
+                m = rearrange(m, "... Bq 1 -> ... Bq")
 
                 updated_max_exp = torch.exp(m_prev - m) # (batch, Bq)
                 # Compute l: Running exp sum per row (batch, Bq) (not accurate bc max is changing)
@@ -67,21 +70,20 @@ class FlashAttention2Pytorch(torch.autograd.Function):
 
 
                 # Compute Oi(j): jth iteration of the ith tile of o (at this point it's the numerator being updated by the new max across the seq)
-                o_tile = einsum(torch.diag(updated_max_exp), o_tile, "... Bq j, ... j d -> ... Bq d") + einsum(p_partial, v_tile, "... Bq Bk, Bk d -> ... Bq d")
+                o_tile = einsum(torch.diag_embed(updated_max_exp), o_tile, "... Bq j, ... j d -> ... Bq d") + einsum(p_partial, v_tile, "... Bq Bk, ... Bk d -> ... Bq d")
             
             # Compute Oi using the latest iteration and normalizing
-            o_tile = einsum(torch.inverse(torch.diag(l)), o_tile, "... Bq i, ... i d -> ... Bq d")
+            o_tile = einsum(torch.inverse(torch.diag_embed(l)), o_tile, "... Bq i, ... i d -> ... Bq d")
             # Write to output
-            output[:, i*Bq:(i+1)*Bq, :] = o_tile
+            o[:, i*Bq:(i+1)*Bq, :] = o_tile
 
             # Compute logsumexp
-            logsumexp[:, i*Bq:(i+1)*Bq] = m + torch.log(l)
+            L[:, i*Bq:(i+1)*Bq] = m + torch.log(l)
+
+        # Save ctx
+        ctx.save_for_backward(L, q, k, v, o)
         
-        return output, logsumexp
-
-
-
-
+        return o
 
 
 
---
+++ flash_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+clear
+
+
+uv run pytest -k test_flash_forward_pass_pytorch
